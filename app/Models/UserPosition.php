ad2c6fbe50a729730defce27a7b15174dba6f54e
Goesde Rai <goesderai@gmail.com>
2024-08-02 18:13:39 +0800

    add relationship to user

--- app/Models/UserPosition.php
+++ app/Models/UserPosition.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class UserPosition extends Model
 {
@@ -13,4 +14,9 @@ class UserPosition extends Model
         'user_id',
         'position_id',
     ];
+
+    public function User(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
